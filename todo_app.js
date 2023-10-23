854f3143a56eb357d3e4c4299451deb0a13e3ead
jungtaekku <dnfqh98@gmail.com>
2023-10-24 00:49:00 +0900

    Feat : d-day 기능 구현 1. 오늘 날짜 설정 시 today 로 d-day 가 설정 2. 오늘 날짜 보다 적은 날은 설정 되지 않음 3. d-day 를 설정 하지 않을 시 d-day 는 빈칸

--- todo_app.js
+++ todo_app.js
@@ -3,22 +3,73 @@ const input = document.querySelector("#addValue");
 const addTag = document.querySelector("#addTag");
 // 경고 메세지
 function warring (){alert("Warring : empty value!")};
+const checkbox = document.createElement("input");
+const button = document.createElement("button");
 
+function dateFomat(){
+    const today = new Date(); 
+    let result = today.getFullYear() + '-' +((today.getMonth()+1)<9?"0"+(today.getMonth()+1) : (today.getMonth()+1))+'-'+
+    ((today.getDate()) < 9 ? "0" +(today.getDate()):(today.getDate()));
+
+    return result;
+}
 //   todo 추가
 function addList()  {
  const addValue = document.querySelector('#addValue').value;
-//  const tagValue = addTag.value;
+    //  const tagValue = addTag.value;
+ const dateValue = document.querySelector('#dateValue').value;
  const itemList = document.getElementById("itemList");
  const li = document.createElement("li");
- const checkbox = document.createElement("input");
  const span = document.createElement("span"); 
- const tag = document.createElement("span");
- const button = document.createElement("button");
+ const day = document.createElement("span");
+ const dday = document.createElement("span");
  const textNode = document.createTextNode(addValue);
+ const today = new Date();
+ const d_day = new Date(dateValue);
+ const timeGap = d_day.getTime() - today.getTime();
+ const remainTime = Math.ceil(timeGap/(1000*60*60*24));
+
+
+ console.log(remainTime);
 
  if(addValue == ""){
    warring();
- }else{
+ }
+ else if(remainTime == 1){
+    span.textContent = addValue;
+    checkbox.type = 'checkbox';
+    button.innerText = 'delete';
+  
+     const addList  = document.getElementById('itemList')
+     .appendChild(li);
+ 
+     addList.appendChild(checkbox);
+     addList.appendChild(span);
+     day.textContent = 'Today';
+     addList.appendChild(day);
+     addList.appendChild(button);
+ 
+    input.value = "";
+ }
+ else if(remainTime <= 0){
+    alert("D-day 는 오늘보다 적은날로 설정할 수 없습니다.");
+ }
+ else if(dateValue == ("" && undefined)){
+
+   span.textContent = addValue;
+   checkbox.type = 'checkbox';
+   button.innerText = 'delete';
+ 
+    const addList  = document.getElementById('itemList')
+    .appendChild(li);
+
+    addList.appendChild(checkbox);
+    addList.appendChild(span);
+    addList.appendChild(button);
+
+   input.value = "";
+ }
+ else{
    // li.setAttribute('id',addValue);
    span.textContent = addValue;
 //    tag.textContent = tagValue;
@@ -30,27 +81,37 @@ function addList()  {
 
     addList.appendChild(checkbox);
     addList.appendChild(span);
+    day.textContent = 'Day-';
+    addList.appendChild(day);
+    // dday.setAttribute('class','dateValue');
+    dday.innerText = remainTime;
+    addList.appendChild(dday);
     // addList.appendChild(tag);
     addList.appendChild(button);
     
-   checkbox.addEventListener('change',(e)=>{
-       if(e.currentTarget.checked){
-           span.style.textDecoration = "line-through";
-       }else {
-           span.style.textDecoration = "none";
-       }
+ 
 
-   })
-
-   button.addEventListener('click',(e)=>{
-       itemList.removeChild(e.currentTarget.parentNode);
-   })
 
    input.value = "";
 //    addTag.value = "";
 
    }
 }
+// 체크박스 체크 시 밑줄
+checkbox.addEventListener('change',(e)=>{
+    if(e.currentTarget.checked){
+        span.style.textDecoration = "line-through";
+    }else {
+        span.style.textDecoration = "none";
+    }
+
+})
+
+// todo 삭제
+button.addEventListener('click',(e)=>{
+    itemList.removeChild(e.currentTarget.parentNode);
+})
+
 // enter 키로 추가
 input.addEventListener('keypress', (event)=>{
    if(event.keyCode === 13){
@@ -100,5 +161,14 @@ const toggleBtn = document.querySelector("#toggle");
 exitBtn.addEventListener('click', function(){
     bar.style.display = "none";
     toggleBtn.style.display = "block";
+    input.value = "";
 })
 
+// function dateCount(value){
+//     const today = new Date();
+//     const dday = new Date(value);
+//     const timeGap = dday.getTime() - today.getTime();
+//     const remainTime = Math.ceil(timeGap/(1000*60*60*24));    
+
+//     return remainTime;
+// }
