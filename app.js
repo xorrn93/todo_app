36de40b99a448eb06d77b658b55c0eb769763ade
jungtaekku <dnfqh98@gmail.com>
2023-10-24 17:13:20 +0900

    node.js 웹서버 구축

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/',(red, res) => {
-    res.send('Hello World');
+    res.sendFile(__dirname + "/public/main.html")
 });
+
+app.get('/main',(red, res) => {
+    res.sendFile(__dirname + "/public/main.html")
+})
+app.use(express.static('public'))
 
 app.listen(port, ()=>{
     console.log(`서버가 실행 됩니다. http://localhost:${port}`);
